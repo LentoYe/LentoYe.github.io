a8a3e6085dff49a2b9ffac93b63d11ce6f398ed4
LentoY2k <yourclosure@icloud.com>
2025-03-28 01:22:49 +0800

    feat: 🎸 测试央视

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p className="lento">
+          Edit <code>hello Lento</code> and save to reload.
+        </p>
+        <p className="lento2">
+          Edit <code>hello Lento2</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
